6ad63d31171f9fa809c34eeb785e2afc760f35b6
traywcr <55033722+traywcr@users.noreply.github.com>
2020-10-25 16:40:07 -0400

    Initial commit buffers are open to modification

---
+++ R13_FIO05_J.java
@@ -0,0 +1,22 @@
+//  Cody Traywick
+//  Rule 13. Input Output (FIO)
+//  FIO05-J. Do not expose buffers or their backing arrays methods to untrusted code
+
+public class R13_FIO05_J
+{
+  final class Wrap
+  {
+    private char[] dataArray;
+
+    public Wrap()
+    {
+      dataArray = new char[10];
+      // Initialize
+    }
+
+    public CharBuffer getBufferCopy()
+    {
+      return CharBuffer.wrap(dataArray);
+    }
+  }
+}
