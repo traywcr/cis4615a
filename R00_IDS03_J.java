88cd4407b58d9b7589ab8d8212b574efdbcb642d
traywcr <55033722+traywcr@users.noreply.github.com>
2020-10-25 14:03:52 -0400

    Initial Commit

---
+++ R00_IDS03_J.java
@@ -0,0 +1,14 @@
+// Cody Traywick
+// Rule 00.  Input Validation and Data Sanitization (IDS)
+
+public class IDS03-J
+{
+  public static void main(string [] args)
+  {
+    if (loginSuccessful) {
+      logger.severe("User login succeeded for: " + username);
+    } else {
+      logger.severe("User login failed for: " + username);
+    }
+  }
+}
