37968097c5ec5b5ecf271a39d6de78b959938a95
traywcr <55033722+traywcr@users.noreply.github.com>
2020-10-25 14:14:03 -0400

    Initial Commit

---
+++ R02_EXP00_J.java
@@ -0,0 +1,12 @@
+// Cody Traywick
+// Rule 02.  Expressions (EXP)
+
+public class IDS03-J
+{
+  public void deleteFile()
+  {
+    File someFile = new File("someFileName.txt");
+    // Do something with someFile
+    someFile.delete();
+  }
+}
