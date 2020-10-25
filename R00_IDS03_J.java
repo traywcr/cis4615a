a2477e4f21230abbcac694d9ee094142f9ffd437
traywcr <55033722+traywcr@users.noreply.github.com>
2020-10-25 14:09:07 -0400

    Sanitize username to prevent injection attacks

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -5,10 +5,18 @@ public class IDS03-J
 {
   public static void main(string [] args)
   {
-    if (loginSuccessful) {
-      logger.severe("User login succeeded for: " + username);
-    } else {
-      logger.severe("User login failed for: " + username);
+    if (loginSuccessful) 
+    {
+      logger.severe("User login succeeded for: " + sanitizeUser(username));
+    } 
+    else 
+    {
+      logger.severe("User login failed for: " + sanitizeUser(username));
+    }
+    public String sanitizeUser(String username)
+    {
+      return Pattern.matches("[A-Za-z0-9_]+", username))
+      ? username : "unauthorized user";
     }
   }
 }
