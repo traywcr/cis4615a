f654f0a37441ee85ec171bafa3539a0416506999
traywcr <55033722+traywcr@users.noreply.github.com>
2020-10-25 17:53:34 -0400

    Adding rule to comment

--- R03_NUM03_J.java
+++ R03_NUM03_J.java
@@ -1,5 +1,6 @@
 // Cody Traywick
 // Rule 03.  Numeric Types and Operations (NUM)
+// NUM03-J. Use integer types that can fully represent the possible range of unsigned data
 
 public class R03_NUM03_J
 {
