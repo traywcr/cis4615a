a80320ccebbc34484b5b8294c4a99e96f3e68e33
traywcr <55033722+traywcr@users.noreply.github.com>
2020-10-25 13:42:23 -0400

    Add sanitization to the username

--- IDS03-J.java
+++ IDS03-J.java
@@ -5,10 +5,19 @@ public class IDS03-J
 {
   public static void main(string [] args)
   {
-    if (loginSuccessful) {
-      logger.severe("User login succeeded for: " + username);
-    } else {
-      logger.severe("User login failed for: " + username);
+    if (loginSuccessful) 
+    {
+      logger.severe("User login succeeded for: " + sanitizeUser(username));
+    } 
+    else 
+    {
+      logger.severe("User login failed for: " + sanitizeUser(username));
     }
+  }
+  // Prevents injection attacks
+  public String sanitizeUser(String username) 
+  {
+  return Pattern.matches("[A-Za-z0-9_]+", username))
+      ? username : "unauthorized user";
   }
 }
