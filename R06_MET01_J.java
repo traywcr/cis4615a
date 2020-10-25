ee0658d40fda99b6fbd91c08542c8950322df8fa
traywcr <55033722+traywcr@users.noreply.github.com>
2020-10-25 18:07:53 -0400

    Reverting to original

--- R06_MET01_J.java
+++ R06_MET01_J.java
@@ -1,20 +1,18 @@
 // Cody Traywick
 // Rule 06.  Methods (MET)
+// MET01-J. Never use assertions to validate method arguments
 
 public class R06_MET01_J
 {
   public static int getAbsAdd(int x, int y) 
   {
-    if (x == Integer.MIN_VALUE || y == Integer.MIN_VALUE) 
-    {
-      throw new IllegalArgumentException();
-    }
+    assert x != Integer.MIN_VALUE;
+    assert y != Integer.MIN_VALUE;
+
     int absX = Math.abs(x);
     int absY = Math.abs(y);
-    if (absX > Integer.MAX_VALUE - absY) 
-    {
-      throw new IllegalArgumentException();
-    }
+
+    assert (absX <= Integer.MAX_VALUE - absY);
     return absX + absY;
   }
 }
