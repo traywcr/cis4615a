273b2ed890c9fa2e9acbf2463e5780199b88317f
traywcr <55033722+traywcr@users.noreply.github.com>
2020-10-25 15:06:44 -0400

    Initial assertion validation commit

---
+++ R06_MET01_J.java
@@ -0,0 +1,17 @@
+// Cody Traywick
+// Rule 06.  Methods (MET)
+
+public class R06_MET01_J
+{
+  public static int getAbsAdd(int x, int y)
+  {
+    assert x != Integer.MIN_VALUE;
+
+    assert y != Integer.MIN_VALUE;
+    int absX = Math.abs(x);
+    int absY = Math.abs(y);
+
+    assert (absX <= Integer.MAX_VALUE - absY);
+    return absX + absY;
+  }
+}
