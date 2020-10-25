e5554b956b3b9e5dc703d9ef5c3eb4b2f3c0f9ca
traywcr <55033722+traywcr@users.noreply.github.com>
2020-10-25 14:30:09 -0400

    Initial vulnerable commit

---
+++ R03_NUM03_J.java
@@ -0,0 +1,10 @@
+// Cody Traywick
+// Rule 03.  Numeric Types and Operations (NUM)
+
+public class R03_NUM03_J
+{
+  public static int getInteger(DataInputStream is) throws IOException
+  {
+    return is.readInt();
+  }
+}
