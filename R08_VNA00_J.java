699f18d92f8261271a30d5e5d3f05e51cbdb3e09
traywcr <55033722+traywcr@users.noreply.github.com>
2020-10-25 16:33:32 -0400

    Ensures writes are visible to all visible threads

--- R08_VNA00_J.java
+++ R08_VNA00_J.java
@@ -6,7 +6,7 @@ public class R08_VNA00_J
 {
   final class ControlledStop implements Runnable
   {
-    private boolean done = false;
+    private volatile boolean done = false;
 
     @Override public void run()
     {
