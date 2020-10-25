182b213c6dde494653d6514cb550d089800ed38e
traywcr <55033722+traywcr@users.noreply.github.com>
2020-10-25 17:46:36 -0400

    Reverting back to unsanitized username

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -1,22 +1,15 @@
 // Cody Traywick
 // Rule 00.  Input Validation and Data Sanitization (IDS)
+// IDS03-J. Do not log unsanitized user input
 
 public class R00_IDS03_J
 {
-  public static void main(string [] args)
+  if (loginSuccessful) 
   {
-    if (loginSuccessful) 
-    {
-      logger.severe("User login succeeded for: " + sanitizeUser(username));
-    } 
-    else 
-    {
-      logger.severe("User login failed for: " + sanitizeUser(username));
-    }
-    public String sanitizeUser(String username)
-    {
-      return Pattern.matches("[A-Za-z0-9_]+", username))
-      ? username : "unauthorized user";
-    }
+    logger.severe("User login succeeded for: " + username);
+  } 
+  else 
+  {
+    logger.severe("User login failed for: " + username);
   }
 }
