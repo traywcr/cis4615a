453cede6d485d9aa47af6714172bbe0b38d105e4
traywcr <55033722+traywcr@users.noreply.github.com>
2020-10-25 18:14:45 -0400

    Checks for null string rather than NullPointerException

---
+++ R07_ERR08_J.java
@@ -0,0 +1,21 @@
+//  Cody Traywick
+//  Rule 07. Exceptional Behavior (ERR)
+//  ERR08-J. Do not catch NullPointerException or any of its ancestors
+
+
+public class R07_ERR08_J
+{
+  boolean isName(String s)
+  {
+    if (s == null)
+    {
+      return false;
+    }
+    String names[] = s.split(" ");
+    if (names.length != 2)
+    {
+      return false;
+    }
+    return (isCapitalized(names[0]) && isCapitalized(names[1]));
+  }
+}
