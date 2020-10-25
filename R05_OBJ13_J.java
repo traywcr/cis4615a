fd732cbe3c5d282de9afb436feeb68830145e3e5
traywcr <55033722+traywcr@users.noreply.github.com>
2020-10-25 18:06:48 -0400

    Reverting to original

--- R05_OBJ13_J.java
+++ R05_OBJ13_J.java
@@ -1,11 +1,9 @@
 // Cody Traywick
 // Rule 05. Object Orientation (OBJ)
+// OBJ13-J. Ensure that references to mutable objects are not exposed
 
 public class R05_OBJ13_J
 {
   public static final SomeType [] SOMETHINGS = { ... };
-  public static final getSomethings() 
-  {
-    return SOMETHINGS.clone();
-  }
+  public static final getSomethings() {return SOMETHINGS;}
 }
