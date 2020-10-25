417bdda9f4ca2104c2545c2fb427b0da8e7ecbaf
traywcr <55033722+traywcr@users.noreply.github.com>
2020-10-25 17:52:08 -0400

    Adding rule to comment

--- R03_NUM02_J.java
+++ R03_NUM02_J.java
@@ -1,5 +1,6 @@
 // Cody Traywick
 // Rule 03 Numeric Types and Operations (NUM)
+// NUM02-J. Ensure that division and remainder operations do not result in divide-by-zero errors
 
 public class R03_NUM02_J
 {
