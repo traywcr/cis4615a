b7da7437dd390b4779d0339322a1cfa5701eb776
traywcr <55033722+traywcr@users.noreply.github.com>
2020-10-25 15:22:08 -0400

    Ensures divide-by-0 never happens

--- R03_NUM02_J.java
+++ R03_NUM02_J.java
@@ -1,11 +1,18 @@
 // Cody Traywick
-// Rule 06.  Methods (MET)
+// Rule 03 Numeric Types and Operations (NUM)
 
 public class R03_NUM02_J
 {
   long num1, num2, result;
-
+ 
   /* Initialize num1 and num2 */
-
-  result = num1 / num2;
+ 
+  if (num2 == 0) 
+  {
+    // Handle error
+  } 
+  else 
+  {
+    result = num1 / num2;
+  }
 }
