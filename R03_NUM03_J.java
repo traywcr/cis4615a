51f7498d24d1dda8459ee621a6e26e036a44f0d2
traywcr <55033722+traywcr@users.noreply.github.com>
2020-10-25 18:04:56 -0400

    Reverting back to bad code

--- R03_NUM03_J.java
+++ R03_NUM03_J.java
@@ -1,10 +1,11 @@
 // Cody Traywick
 // Rule 03.  Numeric Types and Operations (NUM)
+// NUM03-J. Use integer types that can fully represent the possible range of unsigned data
 
 public class R03_NUM03_J
 {
   public static long getInteger(DataInputStream is) throws IOException 
   {
-    return is.readInt() & 0xFFFFFFFFL; // Mask with 32 one-bits
+    return is.readInt();  
   }
 }
