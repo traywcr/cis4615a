406865b892f2dc6f0667a937985b6cd1241c6d09
traywcr <55033722+traywcr@users.noreply.github.com>
2020-10-25 15:54:08 -0400

    Initial exposed return of private array commit

---
+++ R05_OBJ13_J.java
@@ -0,0 +1,9 @@
+// Cody Traywick
+// Rule 05. Object Orientation (OBJ)
+
+public class R05_OBJ13_J
+{
+  public static final SomeType [] SOMETHINGS = { ... };
+  public static final getSomethings() {return SOMETHINGS;}
+
+}
