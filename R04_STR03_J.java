e80569fe1b3198fbbee9b39446739f62f69cebb9
traywcr <55033722+traywcr@users.noreply.github.com>
2020-10-25 18:05:50 -0400

    Reverting to initial code

--- R04_STR03_J.java
+++ R04_STR03_J.java
@@ -1,12 +1,14 @@
 // Cody Traywick
 // Rule 04.  Characters and Strings (STR)
+// STR03-J. Do not encode noncharacter data as a string
+
 
 public class R04_STR03_J
 {
   BigInteger x = new BigInteger("530500452766");
-  String s = x.toString();  // Valid character data
-  byte[] byteArray = s.getBytes();
-
-  String ns = new String(byteArray); 
-  x = new BigInteger(ns); 
+  byte[] byteArray = x.toByteArray();
+  String s = new String(byteArray);
+  
+  byteArray = s.getBytes();
+  x = new BigInteger(byteArray);
 }
